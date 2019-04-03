351849bae8a51eed0c00e59e01876e3737f248bc
Dimitri C <couwenberg@gmail.com>
2019-04-04 07:16:18 +1100

    first commit

---
+++ src/RowComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const RowComponent = () =>
+
+    (<div className="wrapper">
+        <div className="box a1"><select>
+            <option>een</option>
+            <option>twee</option>
+        </select></div>
+        <div className="box a2"><input type="text"></input></div>
+        <div className="box a3"><input type="text"></input></div>
+        <div className="box b1">colspan 2</div>
+  
+
+    </div>)
+
+export default RowComponent
+
