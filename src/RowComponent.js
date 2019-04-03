88434224d06c004eee82cf74e4d9bddb9c58baad
Dimitri C <couwenberg@gmail.com>
2019-04-04 10:25:37 +1100

    2rows hardcoded

--- src/RowComponent.js
+++ src/RowComponent.js
@@ -8,9 +8,15 @@ const RowComponent = () =>
         </select></div>
         <div className="box a2"><input type="text"></input></div>
         <div className="box a3"><input type="text"></input></div>
-        <div className="box b1">colspan 2</div>
-  
+        <div className="box b1">colspan 2 rowspan 2</div>
 
+        <div className="box a1r3"><select>
+            <option>een</option>
+            <option>twee</option>
+        </select></div>
+        <div className="box a2r3"><input type="text"></input></div>
+        <div className="box a3r3"><input type="text"></input></div>
+        <div className="box b1r3">colspan 2 second row</div>
     </div>)
 
 export default RowComponent
