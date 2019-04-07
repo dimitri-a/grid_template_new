fbb36ee7ad8a15381aa8f7ea79bd75f136f62cd2
Dimitri C <couwenberg@gmail.com>
2019-04-07 14:35:49 +1000

    update

--- src/App.js
+++ src/App.js
@@ -3,27 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import RowComponent from './RowComponent';
 
-
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { items: [ 1, 3, 4, 5 ] };
+	}
 
-  constructor(props){
-super(props);
-this.state={items:[1,3,4,5]}
-  }
-
-  addRow=() => {
-    debugger
-    this.state.items.push(5)
-  }
-  render() {
-    
-    return (
-      <div className="App">
-      {this.state.items.map(i => <RowComponent itemNr={i}></RowComponent>)}
-       <button onClick={() => this.addRow()}>Click here</button>
-      </div>
-    );
-  }
+	addRow = () => {
+		debugger;
+		this.state.items.push(5);
+	};
+	render() {
+		return (
+			<div className="App">
+				{this.state.items.map((i) => <RowComponent itemNr={i} />)}
+				<button onClick={() => this.addRow()}>Click here</button>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/RowComponent.js
+++ src/RowComponent.js
@@ -2,14 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 
+// const Wrapper = styled.div`
+// display: grid;
+// grid-gap: 0px;
+//     grid-template-columns: 1fr 1fr 1fr;
+//     // 
+//     background-color: #fff;
+//     color: #444;
+// `
+
 const Wrapper = styled.div`
-display: grid;
-grid-gap: 12px;
-    grid-template-columns: [col] 220px [col] 220px [col] 220px ;
-    grid-template-rows: [row] 50px [row] 50px [row] ;
-    background-color: #fff;
-    color: #444;
-`
+grid-template-columns: 40px 50px auto 50px 40px;
+grid-template-rows: 25% 100px auto;`
 
 const Column1 = styled.div`
     grid-column: col 1 ;
@@ -23,13 +27,17 @@ const Column3 = styled.div`
     grid-column: col 3 ;
     grid-row:row;
 `
+const Column4 = styled.div`
+    grid-column: col 3 ;
+    grid-row:row;
+`
 
 const ColumnSpan2 = styled.div`
     grid-column: 2/4;
     grid-row: row 2 ;
 `
 
-const RowComponent = ({itemNr}) =>
+const RowComponent = ({ itemNr }) =>
     (<Wrapper>
         <Column1>
             <select>
@@ -37,11 +45,12 @@ const RowComponent = ({itemNr}) =>
                 <option>twee</option>
             </select>
         </Column1>
-        <Column2><input type="text" value={itemNr}></input> </Column2>
+        <Column2><input type="text" value="col2"></input> </Column2>
 
         <Column3><input type="text" value="col3"></input></Column3>
+        <Column4>ICON todo</Column4>
 
-        <ColumnSpan2> <textarea>Hello coments here</textarea></ColumnSpan2>
+        {/* <ColumnSpan2> <textarea>Hello coments here</textarea></ColumnSpan2> */}
     </Wrapper>)
 
 export default RowComponent
