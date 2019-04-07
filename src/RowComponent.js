fca3f2755f51dc1d185eb17a41dfcdfacc665f33
Dimitri C <couwenberg@gmail.com>
2019-04-07 17:20:20 +1000

    updated

--- src/RowComponent.js
+++ src/RowComponent.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 const Wrapper = styled.div`
-display:grid
-grid-template-columns: 1fr 1fr 1fr 30px;
-grid-template-rows:25% 35% auto;
-justify-items: start;
-border: solid 1px 	#000000
+display: grid;
+grid-template-columns: 150px  150px  150px 30px;
+grid-template-rows: auto auto; /* changed */
+border: solid 1px #000000;
+grid-gap: 10px; /* grid gap to handle spaces between items if needed */
+padding: 5px; /* space between wrapper and grid items */
 `;
 
-const Column1 = styled.div`grid-column: 1/2;`;
-const Column2 = styled.div`grid-column: 2/3;`;
-const Column3 = styled.div`grid-column: 3/4;`;
-const Column4 = styled.div`grid-column: 4/5;`;
+const Column1 = styled.div`grid-column: 1/2; background-color:yellow`;
+const Column2 = styled.div`grid-column: 2/3; background-color:yellow`;
+const Column3 = styled.div`grid-column: 3/4; background-color:yellow`;
+const Column4 = styled.div`grid-column: 4/5; background-color:yellow`;
 const ColumnSpan2 = styled.div`
-	grid-column: 2/4;
-	grid-row: row 2;
+	grid-column: 2/ span 2;
+    grid-row: 2; /* changed */
+
 `;
+
 
 const RowComponent = ({ itemNr }) => (
 	<Wrapper>
-		<Column1>
+		<Column1 >
 			<select>
 				<option>een</option>
 				<option>twee</option>
 			</select>
 		</Column1>
-		<Column2>
+		<Column2 >
 			<input type="text" value="col2" />{' '}
 		</Column2>
-		<Column3>
+		<Column3 >
 			<input type="text" value="col3" />{' '}
 		</Column3>
-		<Column4>todo iceon </Column4>
+		<Column4 >todo iceon </Column4>
 		<ColumnSpan2>
 			{' '}
 			<textarea>Hello coments here</textarea>
